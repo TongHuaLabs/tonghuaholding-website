5f174f0c634896dfb2647024d1a1b348e2c5c6ed
Parinya Rueangphun <parinya.rueangphun@gmail.com>
2022-06-01 16:03:08 +0700

    style: css config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,23 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
-  theme: {},
+  theme: {
+    extends: {
+      primary: {
+        main: '#CB3939',
+        surface: '#F5D7D7',
+        border: '#EEBDBD',
+        hover: '#A93030',
+        pressed: '#89232B',
+      },
+      brand: {
+        red: '#CB3939',
+        white: '#FFFFFF',
+        grey: '#5E6061',
+      },
+      background: {
+        DEFAULT: '#FFFFFF',
+      },
+    },
+  },
   plugins: [],
 };

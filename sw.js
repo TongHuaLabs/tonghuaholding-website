750926d02fa8ebba303464400b0a6940223b12cf
tehtrippy <parinya.rueangphun@gmail.com>
2022-08-26 17:50:56 +0700

    Updates

--- sw.js
+++ sw.js
@@ -27,20 +27,20 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-6f832e2b589a384fdbc3.js"
+    "url": "webpack-runtime-d2dd812a1416589efc40.js"
   },
   {
     "url": "framework-21f6d315f579be672c71.js"
   },
   {
-    "url": "styles.d12a563815d3346c7c77.css"
+    "url": "styles.6b5dcd77cf8d8a1ba403.css"
   },
   {
-    "url": "app-defedd07c7fd45459829.js"
+    "url": "app-7360ea20f15142920cd7.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "d4da135a302fb90d7b6dc9917661ae0c"
+    "revision": "137d3a2c1ceff176ec05978667aa60e6"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-8a6c6110c8b62135b243.js"
@@ -51,14 +51,10 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "38fc2045da76610449b3fa13bd019f52"
+    "revision": "00a7ec139df1765ad1aa8dd776f0eb1e"
   },
   {
     "url": "polyfill-e311b4e36dd25f0bdb72.js"
-  },
-  {
-    "url": "manifest.webmanifest",
-    "revision": "ac6a659b40f759ffe5719520dc8ed653"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
@@ -150,7 +146,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/tonghuaholding-website/app-defedd07c7fd45459829.js`))) {
+  if (!resources || !(await caches.match(`/tonghuaholding-website/app-7360ea20f15142920cd7.js`))) {
     return await fetch(event.request)
   }
 

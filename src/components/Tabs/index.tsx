c2b8ebf2a5431293b5442406b60d44d7827fae54
tehtrippy <parinya.rueangphun@gmail.com>
2022-08-29 13:07:36 +0700

    refactor: tabs in home page

--- src/components/Tabs/index.tsx
+++ src/components/Tabs/index.tsx
@@ -1,24 +1,42 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Tab } from '@headlessui/react';
+import classNames from 'classnames';
 
 type TabsProps = {
   tabs: {
-    tab: React.ReactNode;
+    title?: string;
   }[];
   components: {
     child: React.ReactNode;
   }[];
   className?: string;
+  tabClassName?: string;
 };
 
-const Tabs: React.FC<TabsProps> = ({ tabs, components, className }) => {
+const Tabs: React.FC<TabsProps> = ({
+  tabs,
+  components,
+  className,
+  tabClassName,
+}) => {
   return (
     <div className={className}>
       <Tab.Group>
         <Tab.List className="flex flex-wrap justify-between items-center overflow-auto">
-          {tabs.map(({ tab }, key) => (
-            <Tab as="div" key={key}>
-              {tab}
+          {tabs.map(({ title }, key) => (
+            <Tab as={Fragment} key={key}>
+              {({ selected }) => (
+                <button
+                  className={classNames(
+                    `px-2 py-1 text-neutral-50 rounded-lg hover:bg-primary-main ${
+                      selected && 'bg-primary-main font-bold'
+                    }`,
+                    tabClassName,
+                  )}
+                >
+                  {title}
+                </button>
+              )}
             </Tab>
           ))}
         </Tab.List>

06c52be727482bb1bcbf6565d6089dce6ddee150
tehtrippy <parinya.rueangphun@gmail.com>
2022-08-31 17:34:07 +0700

    style: fix about page styling

--- src/components/information/Info/index.tsx
+++ src/components/information/Info/index.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 type InfoProps = {
   icon?: React.ReactNode;
   title?: string;
+  description?: string;
   className?: string;
   textClassName?: string;
 };
@@ -11,15 +12,29 @@ type InfoProps = {
 const Info: React.FC<InfoProps> = ({
   icon,
   title,
+  description,
   className,
   textClassName,
 }) => {
   return (
     <div
-      className={classNames(`flex flex-col items-center space-y-4`, className)}
+      className={classNames(
+        `flex flex-col items-center text-center`,
+        className,
+      )}
     >
       {icon}
-      <span className={classNames(`text-lg`, textClassName)}>{title}</span>
+      <span
+        className={classNames(
+          `text-xl mt-10 font-bold text-neutral-900`,
+          textClassName,
+        )}
+      >
+        {title}
+      </span>
+      <p className="text-base mt-2 font-medium text-neutral-700">
+        {description}
+      </p>
     </div>
   );
 };

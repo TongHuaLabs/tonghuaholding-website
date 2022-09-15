775a2d1089f2d44c7091e6d218a08ac111c13a8e
tehtrippy <parinya.rueangphun@gmail.com>
2022-09-15 11:23:34 +0700

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-631d0a53b2d02e390fcb.js"
+    "url": "webpack-runtime-78b917a340b5d56fe235.js"
   },
   {
     "url": "framework-21f6d315f579be672c71.js"
@@ -36,11 +36,11 @@ self.__precacheManifest = [
     "url": "styles.c7b87f36e9550067d66f.css"
   },
   {
-    "url": "app-030c78bce305a858fcc5.js"
+    "url": "app-8c69455b121285a9a5bf.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "f2cb37690f74b2b97da776a2c4917580"
+    "revision": "678c93e8aceb8feb1a420c9f9260d461"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-8a6c6110c8b62135b243.js"
@@ -51,7 +51,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "90fd8142ff4ffe8da5fcb2ab176a97b2"
+    "revision": "4ded12d3633cc0fa581febf9192018d4"
   },
   {
     "url": "polyfill-e311b4e36dd25f0bdb72.js"
@@ -146,7 +146,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/tonghuaholding-website/app-030c78bce305a858fcc5.js`))) {
+  if (!resources || !(await caches.match(`/tonghuaholding-website/app-8c69455b121285a9a5bf.js`))) {
     return await fetch(event.request)
   }
 

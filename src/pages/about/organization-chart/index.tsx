9cc1dfde7de0bc696a3ce689f6c4b880899bcbca
tehtrippy <parinya.rueangphun@gmail.com>
2022-11-02 16:34:01 +0700

    chore: adjust org-chart page content

--- src/pages/about/organization-chart/index.tsx
+++ src/pages/about/organization-chart/index.tsx
@@ -1,16 +1,23 @@
 import PrimarySection from '@/components/sections/PrimarySection';
 import MainLayout from '@/layouts/MainLayout';
+import { graphql, PageProps } from 'gatsby';
 import { StaticImage } from 'gatsby-plugin-image';
+import { useTranslation } from 'gatsby-plugin-react-i18next';
 import React from 'react';
 
-const OrganizationChart: React.FC<{}> = () => {
+type OrganizationChartProps = PageProps<GatsbyTypes.OrganizationChartPageQuery>;
+
+const OrganizationChart: React.FC<OrganizationChartProps> = () => {
+  const { t } = useTranslation();
   return (
     <MainLayout>
-      <PrimarySection title="โครงสร้างองค์กร" />
+      {/* Section 1: โครงสร้างองค์กร */}
+      <PrimarySection title={t('section-1-title')} />
+      {/* Section 2: ณ เดือนกันยายน 2565 */}
       <section className="px-4 pt-10 pb-20 lg:pb-28 md:px-6 lg:px-16 lg:py-20 max-w-7xl mx-auto space-y-10">
-        <h2 className="text-neutral-900 font-bold">ณ เดือนกันยายน 2565</h2>
+        <h2 className="text-neutral-900 font-bold">{t('section-2-title')}</h2>
         <StaticImage
-          src="../../../images/organization-chart.png"
+          src="../../../images/organization-chart.jpeg"
           alt="tonghuaholding organization chart"
           className="w-full"
         />
@@ -20,3 +27,19 @@ const OrganizationChart: React.FC<{}> = () => {
 };
 
 export default OrganizationChart;
+
+export const query = graphql`
+  query OrganizationChartPage($language: String!) {
+    locales: allLocale(
+      filter: { language: { eq: $language }, ns: { eq: "org-chart" } }
+    ) {
+      edges {
+        node {
+          ns
+          data
+          language
+        }
+      }
+    }
+  }
+`;

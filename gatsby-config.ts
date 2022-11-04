51ceff644816ab947410eee14140f00abbb0a206
tehtrippy <parinya.rueangphun@gmail.com>
2022-11-04 16:27:18 +0700

    chore: add favicon

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,7 +3,7 @@ export const siteMetadata = {
   description: ``,
   twitter: `@tonghuaholding`,
   // siteUrl: `https://tonghuaholding.com`, // No trailing slash allowed!
-  siteUrl: `http://localhost:8000`, // No trailing slash allowed!
+  siteUrl: `https://tonghualabs.github.io/`, // for demo
   defaultMetaImage: '/images/meta-default.png', // Path to the image placed in the 'static' folder
 };
 
@@ -46,21 +46,21 @@ export const plugins = [
   `gatsby-plugin-sharp`,
   `gatsby-transformer-sharp`,
   'gatsby-remark-images',
-  // {
-  //   resolve: `gatsby-plugin-manifest`,
-  //   options: {
-  //     name: `Tong Hua Holding`,
-  //     short_name: `Tong Hua Holding`,
-  //     description: 'Tong Hua Holding Website',
-  //     start_url: '/',
-  //     background_color: `#262626`,
-  //     // This will impact how browsers show your PWA/website
-  //     // https://css-tricks.com/meta-theme-color-and-trickery/
-  //     theme_color: `#262626`,
-  //     display: `minimal-ui`,
-  //     icon: `src/images/icon-512x512.png`, // This path is relative to the root of the site.
-  //   },
-  // },
+  {
+    resolve: `gatsby-plugin-manifest`,
+    options: {
+      name: `Tong Hua Holding`,
+      short_name: `Tong Hua Holding`,
+      description: 'Tong Hua Holding Website',
+      start_url: '/',
+      background_color: `#D9231D`,
+      // This will impact how browsers show your PWA/website
+      // https://css-tricks.com/meta-theme-color-and-trickery/
+      theme_color: `#D9231D`,
+      display: `minimal-ui`,
+      icon: `src/images/icon-512x512.png`, // This path is relative to the root of the site.
+    },
+  },
   // this (optional) plugin enables Progressive Web App + Offline functionality
   // To learn more, visit: https://gatsby.dev/offline
   `gatsby-plugin-offline`,

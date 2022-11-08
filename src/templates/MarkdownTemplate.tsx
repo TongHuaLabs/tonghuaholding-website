6092f9ebb1825b5f78211a0a4df630dbaa0bff61
tehtrippy <parinya.rueangphun@gmail.com>
2022-11-08 10:40:23 +0700

    fix: contents

--- src/templates/MarkdownTemplate.tsx
+++ src/templates/MarkdownTemplate.tsx
@@ -10,24 +10,23 @@ type MarkdownTemplateProps = PageProps<GatsbyTypes.MarkdownTemplateQuery>;
 const MarkdownTemplate = ({
   data: { markdownRemark, site },
 }: MarkdownTemplateProps) => {
-  const { siteMetadata } = site || {};
+  const { siteMetadata, pathPrefix } = site || {};
   const { siteUrl } = siteMetadata || {};
   const { html, frontmatter } = markdownRemark || {};
   const { title, description, cover, slug, date } = frontmatter || {};
 
+  const coverImage =
+    cover?.childImageSharp?.gatsbyImageData.images.fallback.src;
+
   return (
     <MainLayout>
-      <Seo
-        title={title || ''}
-        description={description}
-        image={cover?.childImageSharp?.gatsbyImageData.images.fallback.src}
-      />
+      <Seo title={title || ''} description={description} image={coverImage} />
       <section className="relative px-4 md:px-6 lg:px-16 py-10 lg:py-20 max-w-4xl mx-auto">
         <h1 className="font-bold text-2xl lg:text-3xl text-neutral-900">
           {title}
         </h1>
         <h2 className="text-sm text-neutral-600 mt-2">{date}</h2>
-        <ShareButton url={`${siteUrl}${slug}`} className="mt-7" />
+        <ShareButton url={`${siteUrl}${pathPrefix}${slug}`} className="mt-7" />
         {cover?.childImageSharp && (
           <div className="mt-10">
             <div className="!aspect-w-16 !aspect-h-9">
@@ -55,6 +54,7 @@ export default MarkdownTemplate;
 export const pageQuery = graphql`
   query MarkdownTemplate($language: String!, $slug: String!) {
     site {
+      pathPrefix
       siteMetadata {
         siteUrl
       }

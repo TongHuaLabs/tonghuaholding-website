e4e86e2efe4874d662cdc3c1d24a87ec3471d894
tehtrippy <parinya.rueangphun@gmail.com>
2022-11-10 15:04:02 +0700

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-c9639c0553015aa7d90a.js"
+    "url": "webpack-runtime-bec9d0ff2673fb8b84f1.js"
   },
   {
     "url": "framework-a2263c6caa530b74304b.js"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "5051c7f687775147d828c38e69f8b64c"
+    "revision": "7f710c1d16d20e1ad6d70857336757a2"
   },
   {
     "url": "polyfill-fcae02c11a8af2fc94d6.js"
